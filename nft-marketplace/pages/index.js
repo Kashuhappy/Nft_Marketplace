c5528d5cbe2e2277517883e7d3b690982fedb820
Sairikei <84667771+Sairikei@users.noreply.github.com>
2022-03-30 21:23:28 +0300

    app

--- nft-marketplace/pages/index.js
+++ nft-marketplace/pages/index.js
@@ -7,7 +7,45 @@ import {
   nftaddress, nftmarketaddress
 } from '../config'
 
+import NFT from '../artifacts/contracts/NFT.sol/NFT.json'
+import Market from '../artifacts/contracts/Market.sol/NFTMarket.json'
+
 export default function Home() {
+  const [nfts, setNfts] = useState([])
+  const [loadingState, seLoadingState] = useState('not-loaded')
+
+  useEffect(() => {
+    loadedNFTs()
+  }, [])
+
+  async function loadNFTs() {
+    const provider = new ethers.providers.JsonRpcProvider()
+    const tokenContract = new ethers.Contract(nftaddress, NFT.abi, provider)
+    const marketContract = new ethers.Contract(nftmarketaddress, Market.abi, provider)
+    const data = await marketContract.fetchMarketItems()
+
+    const items = await Promise.all(data.map(async i => {
+      const tokenUri = await tokenContract.tokenUri(i.tokenId)
+      const meta = await axios.get(tokenUri)
+      let price = ethers.utils.formatUnits(i.price.toString(), 'ether')
+      let item = {
+        price,
+        tokenId: i.tokenId.toNumber(),
+        seller: i.seller,
+        owner: i.owner,
+        image: meta.data.image,
+        name: meta.data.name,
+        description: meta.data.description,
+      }
+      return item
+    }))
+    setNfts(itmes)
+    setLoadingState('loaded')
+  }
+  if (loadingState === 'loaded' && !nfts.length) return (
+    <h1 className="px-20 py-10 text-3xl">No items in marketplace</h1>
+  )
+
   return (
     <div className={styles.container}>
       <h1>Home</h1>
